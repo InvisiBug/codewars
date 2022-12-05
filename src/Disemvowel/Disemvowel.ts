9161215f5eae3f59f3b522ad6215cfdcb424099c
InvisiBug <matthewjkavanagh@gmail.com>
2022-12-05 16:43:46 +0000

    Added Disemvowel instructions

---
+++ src/Disemvowel/Disemvowel.ts
@@ -0,0 +1,7 @@
+export class Kata {
+  static disemvowel(str: string): string {
+    return str;
+  }
+}
+
+console.log(Kata.disemvowel("This website is for losers LOL!")); // "Ths wbst s fr lsrs LL!"
